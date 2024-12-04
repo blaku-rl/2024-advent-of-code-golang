a150a14b82d9bde693ff4f370442d5ee3e157d8b
blaku <blakuuru@gmail.com>
2024-12-04 00:06:52 -0600

    Almost done with day 2

--- day2/day2.go
+++ day2/day2.go
@@ -13,6 +13,8 @@ var input string
 func main() {
 	fmt.Println("Running part one")
 	partone()
+	fmt.Println("Running part two")
+	parttwo()
 }
 
 func parseInput() [][]int {
@@ -32,27 +34,78 @@ func parseInput() [][]int {
 	return reports
 }
 
+func isReportSafe(report []int) bool {
+	isSafe := true
+	isPositive := report[0] < report[1]
+	for i := 0; i < len(report)-1; i++ {
+		if !isSafeLevelChange(report[i], report[i+1], isPositive) {
+			isSafe = false
+			break
+		}
+	}
+	return isSafe
+}
+
+func isSafeLevelChange(prevLevel int, nextLevel int, isPositive bool) bool {
+	if isPositive && (prevLevel >= nextLevel || prevLevel < nextLevel-3) {
+		return false
+	}
+	if !isPositive && (prevLevel <= nextLevel || prevLevel > nextLevel+3) {
+		return false
+	}
+	return true
+}
+
 func partone() {
 	reports := parseInput()
 	safeReports := 0
 
 	for _, report := range reports {
-		isPositive := report[0] < report[1]
-		isSafe := true
-		for i := range report {
-			if i == len(report)-1 {
-				break
-			}
-			if isPositive && (report[i] >= report[i+1] || report[i] < report[i+1]-3) {
-				isSafe = false
-				break
-			}
-			if !isPositive && (report[i] <= report[i+1] || report[i] > report[i+1]+3) {
-				isSafe = false
-				break
+		if isReportSafe(report) {
+			safeReports++
+		}
+	}
+
+	fmt.Println("Number of safe reports: ", safeReports)
+}
+
+func parttwo() {
+	reports := parseInput()
+	safeReports := 0
+
+	for _, report := range reports {
+		posAmount := 0
+		negAmount := 0
+		for i := 0; i < len(report)-1; i++ {
+			if report[i] < report[i+1] {
+				posAmount++
+			} else if report[i] > report[i+1] {
+				negAmount++
 			}
 		}
-		if isSafe {
+		var isPositive bool
+		if posAmount > negAmount {
+			isPositive = true
+		} else if posAmount < negAmount {
+			isPositive = false
+		} else {
+			continue
+		}
+		unsafeValues := 0
+		for i := 0; i < len(report)-1; i++ {
+			if !isSafeLevelChange(report[i], report[i+1], isPositive) {
+				unsafeValues++
+				if unsafeValues >= 2 || i == len(report)-2 {
+					break
+				}
+				if !isSafeLevelChange(report[i], report[i+2], isPositive) {
+					unsafeValues++
+					break
+				}
+				i++
+			}
+		}
+		if unsafeValues < 2 {
 			safeReports++
 		}
 	}

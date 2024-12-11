bad5335b15cc53d98c270b92ff8615fc42bff918
blaku <blakuuru@gmail.com>
2024-12-11 12:18:57 -0600

    init day10

---
+++ day10/day10.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	fmt.Println("Running part one")
+	partone()
+	fmt.Println("Running part two")
+	parttwo()
+}
+
+func partone() {
+
+}
+
+func parttwo() {
+
+}
